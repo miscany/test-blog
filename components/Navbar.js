8b23a4e94a266649c404de97b7e29a27cf45132d
Matthew Pierce <matt-pierce@hotmail.com>
2022-07-20 08:34:00 -0400

    c

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,13 +1,14 @@
 import React from "react";
 import styled from "styled-components";
 import Link from "next/link";
+import { useState } from "react";
+import { MenuIcon } from "@heroicons/react/solid";
 import COLORS from "../Data/colors";
 
 const NavCont = styled.nav`
   display: flex;
   height: 70px;
-  padding-left: 10%;
-  padding-right: 10%;
+  padding: 0;
   justify-content: space-between;
   align-items: center;
   border-bottom: 1px solid black;
@@ -64,15 +65,52 @@ const Title = styled.h2`
     background-color: #fff;
   }
 `;
+const MobileNav = styled.div`
+  padding: 16px;
+  display: flex;
+  justify-content: flex-end;
+  width: 100%;
+  .nav-hamburger {
+    &:hover {
+      cursor: pointer;
+    }
+  }
+  .lrg-icon {
+    transition: transform 0.25s ease;
+    &:hover {
+      color: ${(props) => props.colors.green};
+      transform: translateY(-2px);
+    }
+  }
+`;
+
+const NavDropdown = styled.div`
+  position: absolute;
+  z-index: 1;
+  top: -100%;
+  height: 100%;
+  width: 100%;
+  left: 0px;
+  transition: top 0.5s ease;
+  background-color: ${(props) => props.colors.darkBlue};
+  .active-dropdown {
+    top: 70px;
+  }
+`;
 const Navbar = () => {
+  const [active, setActive] = useState(false);
+
+  function ToggleDropdown() {
+    setActive(!active);
+  }
   return (
     <NavCont colors={COLORS} name="top">
-      <div className="mobile">
+      <div className="desktop">
         <Link passHref href="/introduction" clasName="link">
           <Title>Primal Enjoyer</Title>
         </Link>
       </div>
-      <NavRight>
+      <NavRight className="tablet">
         <Link passHref href="/" className="link">
           <h3 className="white">Search</h3>
         </Link>
@@ -89,6 +127,17 @@ const Navbar = () => {
           </Link>
         </div>
       </NavRight>
+      <MobileNav colors={COLORS} className="mobile">
+        {(!active && (
+          <MenuIcon
+            onClick={ToggleDropdown}
+            className="lrg-icon nav-hamburger"
+          />
+        )) || <div onClick={ToggleDropdown} className="arrow-up"></div>}
+        <NavDropdown className="active-dropdown" colors={COLORS}>
+          hello
+        </NavDropdown>
+      </MobileNav>
     </NavCont>
   );
 };
